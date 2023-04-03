f6817c40a7018ea2a1c2755d5b40047a0d53d282
mohitmandalia <mohitm@zendrive.com>
2023-04-03 22:56:14 +0530

    Initial Commit

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Mohit Mandalia on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
